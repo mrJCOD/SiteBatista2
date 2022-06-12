9f294aa40a6cfd8a1ff2b2717a0d07864b9ab18a
mrJCOD <mauro_rodrigues15@hotmail.com>
2022-06-11 23:39:16 -0300

    .

--- cypress/e2e/Select.cy.js
+++ cypress/e2e/Select.cy.js
@@ -1,6 +1,7 @@
 describe('empty spec', () => {
     it('passes', () => {
-        cy.visit('https://www.petz.com.br/checkout/cadastro/home')
+        cy.visit('https://www.petshop.com.br/loja/cadastro_layout.php?loja=496822')
+        cy.get('#adicional_0').select('Cachorro').should('have.value', '1')
 
     })
 })
